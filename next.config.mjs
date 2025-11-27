7fd051995be57b06e8a3afa880d632a16bce1d57
unknown <iotheodor@gmail.com>
2025-11-27 11:23:45 +0200

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // basePath: "/portfolio-next-js", ## only used if no custom domain is used...
+  // assetPrefix: "/portoflio-next-js",
+  trailingSlash: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
